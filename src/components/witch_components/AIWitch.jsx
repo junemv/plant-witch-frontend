50609f699f7cf978c3aae39bdf7025f5db676671
Natalya <chshebetun@gmail.com>
2024-08-11 16:48:43 -0700

    Adding styling to the WitchAI component

--- src/components/witch_components/AIWitch.jsx
+++ src/components/witch_components/AIWitch.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './AIWitch.css';
+import plantWitchIcon from '../../plant_witch_icon.png';
+import sendIcon from '../../send-mail-icon.png';
 
 const AIWitch = (props) => {
     const askWitchAI = props.askWitchAI;
@@ -20,20 +22,23 @@ const AIWitch = (props) => {
 return (
 	<div id="ai-witch">
 		<div className="medium-heading">Have Any Plant Questions?</div>
-			<form onSubmit={handleSubmit}>
-				<input className="input-item"
+			<img className="plant-witch-icon" src= {plantWitchIcon} alt="witch-with-plant-icon" />
+			<form className="witch-input-container" onSubmit={handleSubmit}>
+				<textarea className="witch-input-item"
 						type="text"
 						value={prompt}
 						placeholder="Ask me anything about plants!"
 						onChange={handleFormInput}
+						
 						/>
-				<button type="submit">Ask me!</button>
+				<button className="send-btn" type="submit" ><img className="send-button-icon "src={sendIcon} alt="send-mail-icon"/></button>
 			</form>
 
 			{aiResponse && (
-			<div>
+			<div className="witch-response-container">
 				<h3>My witchy response:</h3>
-				<p>{aiResponse.response}</p>
+				<p className="witch-response">{aiResponse.response}</p>
+				<button className="save-response-button" type="submit" >Save to Plant </button>
 			</div>
 			)}
 	</div>
