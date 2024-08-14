d64ee86742f75f1361ff033dcb377b182b5785d4
junemv <vintydesign@gmail.com>
2024-08-13 23:10:20 -0700

    updated images

--- src/components/plant_components/Plant.jsx
+++ src/components/plant_components/Plant.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import './Plant.css';
-import defaultImg from '../../assets/potted-plant-doodle.jpg';
+import defaultImg from '../../assets/default-image.png';
 import shovelIcon from '../../shovel.png'
 import waterCanIcon from '../../watering-can.png';
 import Modal from "../Modal";
@@ -128,8 +128,11 @@ const Plant = (props) => {
 
   useEffect(() => {
     handleWateringAndRepottingStyle(thisPlantsNextWatering, "water-date")
+  }, [thisPlantsNextWatering])
+
+  useEffect(() => {
     handleWateringAndRepottingStyle(thisPlantsNextRepotting, "repot-date")
-  }, [])
+  }, [thisPlantsNextRepotting])
 
   // FORM FUNCTIONS
   const onPlantNameChange = (e) => {
